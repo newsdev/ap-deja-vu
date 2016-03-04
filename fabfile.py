86a52022d215c96c79ce8fac6f7e7d3b630e4877
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-03-03 22:17:27 -0500

    Proper staging host

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ from fabric.state import env
 PROJECT_NAME = 'ap-deja-vu'
 ENVIRONMENTS = {
     "stg": {
-        "hosts": [os.environ.get('AP_DEJAVU_PRD_HOST', None)],
+        "hosts": "int-elex-stg-east.newsdev.net",
     }
 }
 
